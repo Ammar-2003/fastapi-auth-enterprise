d00a1c388fe83d48e8fc6ec23e5194b0da35db8e
Muhammad Ammar <ch.ammar.tech@gmail.com>
2025-06-11 17:59:29 +0500

    Added config file

--- app/main.py
+++ app/main.py
@@ -3,9 +3,22 @@ from typing import Annotated
 from sqlalchemy.orm import Session
 from app.api.v1 import auth
 from app.db.database import get_db
+from sqladmin import Admin, ModelView
+from app.db.models.user import User
+from app.db.database import engine  # your SQLAlchemy engine
+
 app = FastAPI()
 
 
 db_dependency = Annotated[Session, Depends(get_db)]
 
 app.include_router(auth.router, prefix="/api/v1/auth", tags=["Auth"])
+
+# ✅ Setup sqladmin
+admin = Admin(app, engine)
+
+# ✅ Register a model view
+class UserAdmin(ModelView, model=User):
+    column_list = [User.id, User.email, User.full_name]
+
+admin.add_view(UserAdmin)
